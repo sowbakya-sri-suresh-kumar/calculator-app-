ec2ca35f55c444654747e77628fad8b5e93781f9
sowbakya sri <sowbakya1109@gmail.com>
2026-06-18 23:16:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Calculator App");
+function addToResult(value) {
+    document.getElementById("result").value += value;
+}
+
+function clearResult() {
+    document.getElementById("result").value = "";
+}
+
+function calculate() {
+    document.getElementById("result").value =
+        eval(document.getElementById("result").value);
+}
